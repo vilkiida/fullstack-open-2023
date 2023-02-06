24bb0a33b5a045786048d55d565e3b76310e381e
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-02-06 16:04:21 +0200

    1.11

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -11,7 +11,10 @@ const Header = (props) => (
 )
 
 const StatisticLine = ({ text, value }) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   if (all === 0) {
@@ -20,14 +23,14 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
     )
   }
   return (
-  <div>
+  <table>
     <StatisticLine text="good" value={good}/>
     <StatisticLine text="neutral" value={neutral}/>
     <StatisticLine text="bad" value={bad}/>
     <StatisticLine text="all" value={all}/>
     <StatisticLine text="average" value={average}/>
     <StatisticLine text="positive" value={positive}/>
-  </div>
+  </table>
 )}
 
 const App = () => {
