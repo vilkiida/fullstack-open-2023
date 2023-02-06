5b090f8d20ea9c4ed7ead5d783b1f07bf4488903
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-02-06 15:38:39 +0200

    1.7

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -10,11 +10,14 @@ const Header = (props) => (
   <h1>{props.text}</h1>
 )
 
-const Results = ({ good, neutral, bad }) => (
+const Results = ({ good, neutral, bad, all, average, positive }) => (
   <div>
     <p>good {good}</p>
     <p>neutral {neutral}</p>
     <p>bad {bad}</p>
+    <p>all {all}</p>
+    <p>average {average}</p>
+    <p>positive {positive}%</p>
   </div>
 )
 
@@ -22,6 +25,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good*1 + neutral*0 + bad*-1)/all
+  const positive = all === 0 ? 0 : (good/all) * 100
 
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
@@ -37,7 +43,7 @@ const App = () => {
       </div>
       <div>
         <Header text='statistics'/>
-        <Results good={good} neutral={neutral} bad={bad}/>
+        <Results good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
       </div>
     </div>
   )
