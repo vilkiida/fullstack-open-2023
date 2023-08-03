63aaa7a76ddc8f408cffc8fa92d3b8c43302f08d
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-08-03 16:37:33 +0300

    2.4

--- osa2/kurssitiedot/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/kurssitiedot/src/App.js
@@ -5,33 +5,60 @@ const Course = ({ course }) => (
   </div>
 )
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <div>
-      <Course course={course} />
-      <Total parts={course.parts} />
-    </div>
+    <ul>
+      {courses.map(course =>
+        <div key={course.id}>
+          <Course course={course} />
+          <Total parts={course.parts} />
+        </div>
+      )}
+    </ul>
   )
 }
 
@@ -46,9 +73,9 @@ const Content = ({ parts }) => {
   return (
     <ul>
       {parts.map(part => 
-        <li key={part.id}>
+        <div key={part.id}>
           <Part part={part} />
-        </li>
+        </div>
       )}
     </ul>
   )
